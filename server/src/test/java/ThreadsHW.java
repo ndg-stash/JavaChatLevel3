f7dca198194997f08518ed235e7d7b4640b763bb
Kreat <kreator1989@ya.ru>
2021-02-28 21:01:17 +0300

    ThreadsHW

--- server/src/test/java/ThreadsHW.java
+++ server/src/test/java/ThreadsHW.java
@@ -1,34 +1,80 @@
 public class ThreadsHW {
 
-    final static int cyclesCount = 5;
-    final static char[] chars = new char[]{'A','B','C'};
-    static volatile int idx = 0;
+    final static int cyclesCount = 100;
+    final static char[] chars = new char[]{'A','B','C','D','E','F','G','H','I','J','K'};
+    static int idx = 0;
 
     public static void main(String[] args) {
 
         new Thread(() -> {
             for (int i = 0; i < cyclesCount; i++) {
-                printChars();
+                printCharsSync();
             }
         }).start();
 
         new Thread(() -> {
             for (int i = 0; i < cyclesCount; i++) {
-                printChars();
+                printCharsSync();
             }
         }).start();
 
         new Thread(() -> {
             for (int i = 0; i < cyclesCount; i++) {
-                printChars();
+                printCharsSync();
             }
         }).start();
+
+        //Почему при использовании слипа они уже синхронизируются?
+//        new Thread(() -> {
+//            try {
+//                Thread.sleep(1);
+//                for (int i = 0; i < cyclesCount; i++) {
+//                    printChars();
+//                }
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }).start();
+//
+//        new Thread(() -> {
+//            try {
+//                Thread.sleep(1);
+//                for (int i = 0; i < cyclesCount; i++) {
+//                    printChars();
+//                }
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }).start();
+//
+//        new Thread(() -> {
+//            try {
+//                Thread.sleep(1);
+//                for (int i = 0; i < cyclesCount; i++) {
+//                    printChars();
+//                }
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }).start();
+
     }
 
-    static synchronized void printChars(){
+    static void printChars(){
         System.out.print(chars[idx]);
         //idx = idx == 2 ? 0 : idx + 1;
-        if (idx < 2){
+        if (idx < chars.length-1){
+            idx++;
+        } else{
+            idx = 0;
+            System.out.println();
+        }
+    }
+
+    static synchronized void printCharsSync(){
+        System.out.print(chars[idx]);
+        //idx = idx == 2 ? 0 : idx + 1;
+        if (idx < chars.length-1){
             idx++;
         } else{
             idx = 0;
